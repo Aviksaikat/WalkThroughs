966dd79689b2431c3174455fa2539edaef7a4ce0
Aviksaikat <saikickkarma@gmail.com>
2021-01-19 20:04:59 +0530

    KOTH & chocolateFactory added

---
+++ TryHackMe/chocolateFactory/root.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+from cryptography.fernet import Fernet
+import pyfiglet
+
+key = input("Enter the key:  ")
+
+f = Fernet(key)
+
+encrypted_mess = 'gAAAAABfdb52eejIlEaE9ttPY8ckMMfHTIw5lamAWMy8yEdGPhnm9_H_yQikhR-bPy09-NVQn8lF_PDXyTo-T7CpmrFfoVRWzlm0OffAsUM7KIO_xbIQkQojwf_unpPAAKyJQDHNvQaJ'
+dcrypt_mess = f.decrypt(encrypted_mess)
+mess = dcrypt_mess.decode()
+
+display1 = pyfiglet.figlet_format("You Are Now The Owner Of ")
+display2 = pyfiglet.figlet_format("Chocolate Factory ")
+
+print(display1)
+print(display2)
+print(mess)
---
+++ TryHackMe/koth/production/README.md
@@ -0,0 +1,28 @@
+# TryHackMe Koth Production
+
+
+# Saikat Karmakar | Jan 18:2021
+
+***1st machine***
+ip = 10.10.173.26
+
+1. flag1
+```
+cde6951cf12ff485d6d33ad7a2e6ac49
+```
+2. flag2
+```
+06380baf84b7f9a8161e1642d4771251
+```
+3. flag3
+```
+04461ad0759944a4d743deec6bbd8d54
+```
+4. flag4
+```
+eabe4da21f519b8d6726427df7e683c5
+```
+
+
+
+
